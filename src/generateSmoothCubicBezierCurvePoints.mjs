2fed90ad57acdfb4d79b67852e2ae835cc84e438
quan <quan_xiaoxiao@qq.com>
2025-02-07 15:27:39 +0800

    xxx

--- src/generateSmoothCubicBezierCurvePoints.mjs
+++ src/generateSmoothCubicBezierCurvePoints.mjs
@@ -6,12 +6,11 @@ export default (
   x2,
   y2,
 ) => {
-
-  const numPoints = 80;
+  const n = 80;
 
   const points = [];
-  for (let i = 0; i < numPoints; i++) {
-    const t = i / (numPoints - 1);
+  for (let i = 0; i < n; i++) {
+    const t = i / (n - 1);
     const x = Math.pow(1 - t, 2) * x0 + 2 * (1 - t) * t * x1 + Math.pow(t, 2) * x2;
     const y = Math.pow(1 - t, 2) * y0 + 2 * (1 - t) * t * y1 + Math.pow(t, 2) * y2;
     points.push([x, y]);
