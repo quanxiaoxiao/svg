99b7b301a65461171a7e4348e24c61fc555486d7
quan <quan_xiaoxiao@qq.com>
2025-02-07 15:36:40 +0800

    xxx

--- src/generateSmoothQuadraticCurvePoints.mjs
+++ src/generateSmoothQuadraticCurvePoints.mjs
@@ -4,13 +4,13 @@ export default (
   x1,
   y1,
 ) => {
-  const numPoints = 80;
+  const n = 80;
   const cx = 2 * x0 - x1;
   const cy = 2 * y0 - y1;
 
   let points = [];
-  for (let t = 0; t < numPoints; t++) {
-    const tNorm = (t + 1) / numPoints;
+  for (let t = 0; t < n ; t++) {
+    const tNorm = (t + 1) / n;
     const xt = (1 - tNorm) * (1 - tNorm) * x0 + 2 * (1 - tNorm) * tNorm * cx + tNorm * tNorm * x1;
     const yt = (1 - tNorm) * (1 - tNorm) * y0 + 2 * (1 - tNorm) * tNorm * cy + tNorm * tNorm * y1;
     points.push([xt, yt]);
