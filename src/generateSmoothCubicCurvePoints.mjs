e90e93514e218362ee65687e2f3770f7f945058c
quan <quan_xiaoxiao@qq.com>
2025-02-16 16:31:56 +0800

    xxx

--- src/generateSmoothCubicCurvePoints.mjs
+++ src/generateSmoothCubicCurvePoints.mjs
@@ -1,6 +1,6 @@
 const bezier = (t, p0, p1, p2) => {
-  const x = (1 - t) * (1 - t) * p0.x + 2 * (1 - t) * t * p1.x + t * t * p2.x;
-  const y = (1 - t) * (1 - t) * p0.y + 2 * (1 - t) * t * p1.y + t * t * p2.y;
+  const x = Math.pow(1 - t, 2) * p0[0] + 2 * (1 - t) * t * p1[0] + Math.pow(t, 2) * p2[0];
+  const y = Math.pow(1 - t, 2) * p0[1] + 2 * (1 - t) * t * p1[1] + Math.pow(t, 2) * p2[1];
   return [x, y];
 };
 
@@ -15,12 +15,17 @@ export default (
   const n = 120;
 
   const points = [];
+  const startPoint = [x0, y0];
+  const controlPoint = [x1, y1];
+  const endPoint = [x2, y2];
   for (let i = 0; i < n; i++) {
     const t = i / (n - 1);
-    const x = Math.pow(1 - t, 2) * x0 + 2 * (1 - t) * t * x1 + Math.pow(t, 2) * x2;
-    const y = Math.pow(1 - t, 2) * y0 + 2 * (1 - t) * t * y1 + Math.pow(t, 2) * y2;
-    points.push([x, y]);
-    // points.push(bezier(t, [x0, y0], [x1, y1], [x2, y2]));
+    points.push(bezier(
+      t,
+      startPoint,
+      controlPoint,
+      endPoint,
+    ));
   }
 
   return points;
